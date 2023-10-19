dac24e60d020b98c16456cfe4545e3cc5ef394d7
Wilberson Rocha <wilrocha@gmail.com>
2023-10-19 12:28:29 -0300

    Inicital Commit

---
+++ src/Transpose-Matrix/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Transpose_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows, cols;
+
+            int[,] matrix = new [,] {
+                        {1,2,3},
+                        {4,5,6},
+                        {7,8,9} };
+            rows = matrix.GetLength(0);
+            cols = matrix.GetLength(1);
+
+            int[,] transposed = new int[cols, rows];
+
+            Console.WriteLine("Given matrix: ");
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    int value = matrix[i, j];
+                    Console.Write(string.Concat(value, " "));
+
+                    transposed[i, j] = matrix[j, i];
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(Environment.NewLine + "Transpose of given matrix: ");
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    int value = transposed[i, j];
+                    Console.Write(string.Concat(value, " "));
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
